8cf0bdaf684be7497439e9390957d66703c324ef
siranjeevi1316 <siranjeevimech10@gmail.com>
2025-04-22 12:43:53 +0530

    Test Run4

---
+++ src/test/java/org/test/Test2.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test5");
+		System.out.println("Test6");
+		System.out.println("Test7");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
